609347d0dd422538cb4aaa954de2928d4ca57542
HQ <Spawn@calvisitor-10-105-170-140.calvisitor.1918.berkeley.edu>
2017-06-27 09:27:23 -0700

    Initializing files and folders

---
+++ assets/javascript/mechanics.js
@@ -0,0 +1,14 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyCO0APye8L4Bce2-PiG9aCUu2FRt2ikS1w",
+	authDomain: "rps-homework.firebaseapp.com",
+	databaseURL: "https://rps-homework.firebaseio.com",
+	projectId: "rps-homework",
+	storageBucket: "rps-homework.appspot.com",
+	messagingSenderId: "151540006241"
+};
+
+firebase.initializeApp(config);
+
+
+
