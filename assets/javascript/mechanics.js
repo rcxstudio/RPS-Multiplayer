b83f66b2a32404c3527679380ecea1bd1adc45bf
HQ <Spawn@ROPERs-MacBook-Pro.local>
2017-06-30 00:36:13 -0700

    Coming along

--- assets/javascript/mechanics.js
+++ assets/javascript/mechanics.js
@@ -27,7 +27,9 @@ var player1Toggle = false;
 var player2Toggle = false;
 
 var player1Info;
+var player1Choice;
 var player2Info;
+var player2Choice;
 
 //CONTROL TEST
 console.log('player1Database - before', player1Database.child)
@@ -147,7 +149,7 @@ $('#add-player').on('click', function(event) {
 
     else if (player1Toggle != false) {
         addPlayer2();
-        player1DisplayInfo();
+        $('#player1-name-display').text('Player 1: ' + player1Info.name);
         player2DisplayInfo();
 
         $('#player').text(player2Info.name);
@@ -156,9 +158,41 @@ $('#add-player').on('click', function(event) {
     }
 });
 
+
+
+
+// FIX CLICK SELECTIONS-----------------
+$(document).on('click', '.player1choices', function(event) {
+    console.log('clicked');
+    player1choice = $(this).text();
+    console.log(player1choice);
+
+    player1Database.push({
+        choice: player1Choice
+    });
+    results();
+});
+
+$(document).on('click', '.player2choices', function(event) {
+    console.log('clicked');
+    player2choice = $(this).text();
+    console.log(player2choice);
+
+    player2Database.push({
+        choice: player2Choice
+    });
+    results();
+});
+// FIX CLICK SELECTIONS-----------------
+
+
+
+
 //watches for any change at all
 database.ref().on('value', function(snapshot) {
     // Log everything that's coming out of snapshot
+
+    //WAYS TO REFER TO FIREBASE DATA ---KEEPING FOR REFERENCE IN FUTURE
     console.log(snapshot.val()); //starts as null
     console.log('is snapshot.val() equal to null', snapshot.val() === null);
     console.log('snapshot not false', snapshot.val() != null);
@@ -186,6 +220,15 @@ database.ref().on('value', function(snapshot) {
         console.log(player2Toggle);
     }
 
+    // WORK IN PROGRESS SAVING - START
+    // else if (player2Presence === 'player2') {
+    //     player1Toggle = true;
+    //     player1Info = (Object.values(snapshot.val())[0]).player1;
+    //     console.log('snapshot of player1', (Object.values(snapshot.val())[0]).player2);
+    //     console.log(player2Info);
+    // }
+    // WORK IN PROGRESS SAVING - END
+
     // else if (snapshat.val() === null) {
     //     player1Presence = false;
     //     player2Presence = false;
@@ -197,106 +240,8 @@ database.ref().on('value', function(snapshot) {
 );
 
 
-// database.ref().on('child_added', function(snapshot) {
-//     if snapshot.child("name").val(); // {first:"Ada",last:"Lovelace"}
-//     var firstName = snapshot.child("name/first").val(); // "Ada"
-//     var lastName = snapshot.child("name").child("last").val(); // "Lovelace"
-//     var age = snapshot.child("age").val(); // null
-//     });
-
-
-
-//     if ((player1 === undefined && player2 === undefined) || (player1 === undefined && player2 != undefined)) {
-//         player1 = $("#player-name-input").val().trim();
-//         //console.log('player1', player1);
-//         // comment = $("#comment-input").val().trim();
-
-//         // Code for "Setting values in the database" --- MAYBE SET IN SESSION STORAGE SO IT STAYS ON?
-//         player1data.set({
-//             playerName: player1,
-//             //comment: 
-//         });
-
-//         $('#player1-name-display').text('Player 1: ' + player1);
-//         $('#player1-box').append('<p class="player1choices">Rock</p>');
-//         $('#player1-box').append('<p class="player1choices">Paper</p>');
-//         $('#player1-box').append('<p class="player1choices">Scissors</p>');
-//     }
-
-//     //MIGHT HAVE AN ISSUE WITH LINE BELOW---RELOAD OF PAGE ALWAYS MAKES THIS UNDEFINED
-//     else if (player1 != undefined && player2 === undefined) {
-//         player2 = $("#player-name-input").val().trim();
-//         //console.log('player2', player2);
-
-//         // Code for "Setting values in the database"
-//         player2data.set({
-//             playerName: player2,
-//             //comment: 
-//         });
-
-//         $('#player2-name-display').text('Player 2: ' + player2);
-//         $('#player2-box').append('<p class="player2choices">Rock</p>');
-//         $('#player2-box').append('<p class="player2choices">Paper</p>');
-//         $('#player2-box').append('<p class="player2choices">Scissors</p>');
-//     }
-
-//     console.log('player1data - after click', player1data.key.playerName);
-
-// });
-
-// $(document).on('click', '.player1choices', function(event) {
-//     // console.log('clicked');
-//     player1choice = $(this).text();
-//     // console.log(player1choice);
-//     results();
-
-// });
-
-// $(document).on('click', '.player2choices', function(event) {
-//     // console.log('clicked');
-//     player2choice = $(this).text();
-//     // console.log(player2choice);
-//     results();
-// });
-
-
-// database.ref().on('child_added', function(snapshot) {
-//   // if () {
-
-//       // Log everything that's coming out of snapshot
-//       console.log(snapshot.val());
-//       console.log(snapshot.val().playerName);
-//       // console.log(snapshot.val(player1Name));
-
-//       // Change the HTML to reflect player leaving (Store playername in sessions)
-//       // $("#name-display").html(snapshot.val().name);
-//   // }
-
-
-
-
-
-//     // Handle the errors
-//     }, function(errorObject) {
-//       console.log("Errors handled: " + errorObject.code);
-// });
-
-// database.ref().on('child_removed', function(snapshot) {
-
-//       // Log everything that's coming out of snapshot
-//       console.log(snapshot.val());
-//       console.log(snapshot.val().playerName);
-
-//       // Change the HTML to reflect player leaving (Store playername in sessions)
-//       // $("#name-display").html(snapshot.val().name);
-
-//       // Handle the errors
-//     }, function(errorObject) {
-//       console.log("Errors handled: " + errorObject.code);
-// });
-
+//WORK ON EDITING THE BELOW CODE!!!!!!!!!!!!!!!!
 // function results() {
-
 //     if ((player1choice != undefined) && (player2choice != undefined)) {
 //         var player1win = $('#results-box').html('<p>' + player1 + ' wins with ' + player1choice + '</p>');
 //         var player2win = $('#results-box').html('<p>' + player2 + ' wins with ' + player2choice + '</p>');
@@ -347,6 +292,93 @@ database.ref().on('value', function(snapshot) {
 //         $('#results-box').html('<p>Waiting for ' + player1 + ' to choose.</p>');
 //     }
 // }
+//WORK ON ABOVE CODE!!!!!
+
+// database.ref().on('child_added', function(snapshot) {
+//     if snapshot.child("name").val(); // {first:"Ada",last:"Lovelace"}
+//     var firstName = snapshot.child("name/first").val(); // "Ada"
+//     var lastName = snapshot.child("name").child("last").val(); // "Lovelace"
+//     var age = snapshot.child("age").val(); // null
+//     });
+
+
+
+//     if ((player1 === undefined && player2 === undefined) || (player1 === undefined && player2 != undefined)) {
+//         player1 = $("#player-name-input").val().trim();
+//         //console.log('player1', player1);
+//         // comment = $("#comment-input").val().trim();
+
+//         // Code for "Setting values in the database" --- MAYBE SET IN SESSION STORAGE SO IT STAYS ON?
+//         player1data.set({
+//             playerName: player1,
+//             //comment: 
+//         });
+
+//         $('#player1-name-display').text('Player 1: ' + player1);
+//         $('#player1-box').append('<p class="player1choices">Rock</p>');
+//         $('#player1-box').append('<p class="player1choices">Paper</p>');
+//         $('#player1-box').append('<p class="player1choices">Scissors</p>');
+//     }
+
+//     //MIGHT HAVE AN ISSUE WITH LINE BELOW---RELOAD OF PAGE ALWAYS MAKES THIS UNDEFINED
+//     else if (player1 != undefined && player2 === undefined) {
+//         player2 = $("#player-name-input").val().trim();
+//         //console.log('player2', player2);
+
+//         // Code for "Setting values in the database"
+//         player2data.set({
+//             playerName: player2,
+//             //comment: 
+//         });
+
+//         $('#player2-name-display').text('Player 2: ' + player2);
+//         $('#player2-box').append('<p class="player2choices">Rock</p>');
+//         $('#player2-box').append('<p class="player2choices">Paper</p>');
+//         $('#player2-box').append('<p class="player2choices">Scissors</p>');
+//     }
+
+//     console.log('player1data - after click', player1data.key.playerName);
+
+// });
+
+
+
+// database.ref().on('child_added', function(snapshot) {
+//   // if () {
+
+//       // Log everything that's coming out of snapshot
+//       console.log(snapshot.val());
+//       console.log(snapshot.val().playerName);
+//       // console.log(snapshot.val(player1Name));
+
+//       // Change the HTML to reflect player leaving (Store playername in sessions)
+//       // $("#name-display").html(snapshot.val().name);
+//   // }
+
+
+
+
+
+//     // Handle the errors
+//     }, function(errorObject) {
+//       console.log("Errors handled: " + errorObject.code);
+// });
+
+// database.ref().on('child_removed', function(snapshot) {
+
+//       // Log everything that's coming out of snapshot
+//       console.log(snapshot.val());
+//       console.log(snapshot.val().playerName);
+
+//       // Change the HTML to reflect player leaving (Store playername in sessions)
+//       // $("#name-display").html(snapshot.val().name);
+
+//       // Handle the errors
+//     }, function(errorObject) {
+//       console.log("Errors handled: " + errorObject.code);
+// });
+
+
 
 // function playerChoiceClear() {
 //     player1choice = undefined;
